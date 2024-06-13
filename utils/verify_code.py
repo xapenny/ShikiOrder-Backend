aaa0fda4269af8001b5ef3c4fdd4c859110f45fa
Shiona <xapenny@icloud.com>
2024-06-13 10:12:16 +0800

    Update: verification code generate method

--- utils/verify_code.py
+++ utils/verify_code.py
@@ -1,3 +1,5 @@
+import string
+import random
 from datetime import datetime
 
 
@@ -7,7 +9,12 @@ class VerifyCode:
     date = datetime.now().date()
 
     @classmethod
-    def get_code(cls, is_takeout: bool):
+    def get_code(cls, is_takeout):
+        prefix = 'A' if is_takeout else 'B'
+        return prefix + ''.join(random.choices(string.digits, k=4))
+
+    @classmethod
+    def get_code_old(cls, is_takeout: bool):
         if cls.date != datetime.now().date():
             cls.date = datetime.now().date()
             cls.code_A = 0
